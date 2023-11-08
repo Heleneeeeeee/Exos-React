cb2b594fd93b3efa23ef3f72dc34e8c7f41b1463
Helene <helene.quintard@lapiscine.pro>
2023-11-08 10:11:34 +0100

    Add Connection

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,6 @@
 function Header () {
+const isUserLogged = true;
+
     return (
         <header>
             <h1>Mon super site d'E-commerce</h1>
@@ -7,6 +9,12 @@ function Header () {
                     <li>Home</li>
                     <li>Products</li>
                     <li>Contact</li>
+            {isUserLogged?(
+            <li>Hélène QUINTARD</li>
+            ):(
+            <li>Connection</li>
+            )}
+            
                 </ul>
             </nav>
         </header>
--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import Products from './Products';
 function Home () {
     return (
         <div>
-
+            <h2> Supers produits</h2>
             <Products />
         </div>
     );
