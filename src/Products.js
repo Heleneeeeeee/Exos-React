9dcea8894582e4029450fb0d63946397c0e3522a
Helene <helene.quintard@lapiscine.pro>
2023-11-08 13:53:25 +0100

    Add Array objects

--- src/Products.js
+++ src/Products.js
@@ -1,13 +1,31 @@
 function Products () {
 
-    const productsAvailable = ["Robe", "Pull", "Pantalon"];
+    const productsAvailable = [
+        {title:"Robe",
+         price: 170,
+         isPublished:"true",
+        },
+        {title:"Pull",
+         price: 50,
+         isPublished:"true",
+        },
+        {title:"Pantalon",
+         price: 120,
+         isPublished:"true",
+        },
+        {title:"Chemise",
+         price: 100,
+         isPublished:"true",
+        },
+    ];
 
     return (
        <main>
         {productsAvailable.map((product) => {
         return (
         <article>
-            <h2>{product}</h2>
+            <h2>{product.title}</h2>
+            <p>{product.price}€</p>
         </article>
         );
             })}
