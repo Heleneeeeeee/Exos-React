ecb3ba80466c49e1641f4deb5e952eafdb3f897c
Helene <helene.quintard@lapiscine.pro>
2023-11-08 10:42:12 +0100

    Add Cart

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,26 @@
 function Header () {
 const isUserLogged = true;
+const itemsInCart = 2;
 
     return (
         <header>
             <h1>Mon super site d'E-commerce</h1>
             <nav>
                 <ul>
+                    {isUserLogged?(
+                    <li>Hélène QUINTARD</li>
+                    ):(
+                    <li>Connexion</li>
+                    )}  
                     <li>Home</li>
                     <li>Products</li>
                     <li>Contact</li>
-            {isUserLogged?(
-            <li>Hélène QUINTARD</li>
-            ):(
-            <li>Connection</li>
-            )}
+                    {itemsInCart > 0 ?(
+                    <li>Cart : {itemsInCart}</li>
+                    ):(
+                    <li>Pas d'item dans le panier</li>
+                    )}
+            
             
                 </ul>
             </nav>
